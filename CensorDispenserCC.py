c626ac34478f54d9c83fb5b98f1545bfb1abc7e4
Charvizard <42737837+Charvizard@users.noreply.github.com>
2020-09-01 16:23:46 +0200

    Fixed broken stuff

--- CensorDispenserCC.py
+++ CensorDispenserCC.py
@@ -3,70 +3,121 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+punctuation = [",", "!", "?", ".", "%", "/", "(", ")"]
+
+def censor_word(input_text, censor) :
+    censored_item = "" 
+    for x in range(0, len(censor)) : 
+        if censor[x] == " ": 
+            censored_item = censored_item + " "
+        else : 
+            censored_item = censored_item + "-"
+    return input_text.replace(censor, censored_item)
+
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+def censor_proprietary_terms(input_text, censored_list) : 
+    for word in censored_list : 
+        censored_word = ""
+        for x in range(0,len(word)) : 
+            if word[x] == " " : 
+                censored_word = censored_word + " "
+            else : 
+                censored_word = censored_word + "-"
+        input_text = input_text. replace(word, censored_word)
+    return input_text
+
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-def censor_word(text, word) : 
-    word = word.lower() 
-    index = text.find(word)
-    if index <= 0 :
-        return print("The word was not found in the text")
-    else :
-        censored_email_fractured = text.split(word)
-        censored_email = "------".join(censored_email_fractured)
-        return censored_email
+def censor_negative_word(input_text, censored_list, negative_words) : 
+    input_text_words = [] 
+    # Cleans up the words and puts it into a new array
+    for x in input_text.split(" ") : 
+        x1 = x.split("\n")
+        for word in x1:
+            input_text_words.append(word)
+    #  Censor : goes through the new array word for word
+    for i in range(0, len(input_text_words)) : 
+        # Checks if the word is in the censor list and replaces it with a string of equal length if it exists
+        if (input_text_words[i] in censored_list) == True: 
+            word_clean = input_text_words[i]
+   
+            censored_word = ""
+            for x in range(0, len(word_clean)) : 
+                censored_word = censored_word + "-"
+            input_text_words[i] = input_text_words[i].replace(word_clean, censored_word)
+        # initiates a variable to count the amount of times a word appears" 
+        count = 0
+        for i in range(0, len(input_text_words)) : 
+            # checks if the word is in the negative list 
+            if (input_text_words[i] in negative_words) == True : 
+                count += 1 
+                # Only censors if the word appears more than once in the text
+                if count > 2 : 
+                    word_clean = input_text_words[i]
+                    #strips any punctuation off of the word
+                    for x in punctuation : 
+                        word_clean = word_clean.strip(x)
+                    censored_word = ""
+                    for x in range(0, len(word_clean)) : 
+                        censored_word = censored_word + "-"
+                    input_text_words[i] = input_text_words[i].replace(word_clean,censored_word)
+    return " ".join(input_text_words)
+    
+def censor_negative_word_two(input_text, censored_list, negative_words)  :  
+   email_to_censor = censor_proprietary_terms(input_text, censored_list)
+   negative_word_count = 0
+   for word in negative_words :
+      word.lower()
+      if email_to_censor.find(word) >= 0 : 
+         negative_word_count += 1
+         if negative_word_count > 2 : 
+           email_to_censor = email_to_censor.replace(word, "------")
+   return email_to_censor
 
-def censor_proprietary_terms(term_list, text) : 
-    email_to_censor = text.lower()
-    for term in term_list : 
-        term.lower()
-        email_to_censor = email_to_censor.replace(term, "------")
-    return email_to_censor
+def censor_everything(input_text, censored_list) : 
+    input_text_words = [] 
+    for x in input_text.split(" ") : 
+        x1 = x.split("\n") 
+        for word in x1: 
+            input_text_words.append(word)
+    for i in range(0, len(input_text_words)) : 
+        checked_word = input_text_words[i].lower() 
+        for x in punctuation : 
+            checked_word = checked_word.strip(x) 
+        if checked_word in censored_list : 
 
-def censor_negative_word(term_list, negative_word_list, text) : 
-    email_to_censor = censor_proprietary_terms(term_list, text)
-    negative_word_count = 0
-    print(email_to_censor)
-    print("--------------------------------------")
-    for word in negative_word_list :
-       word.lower()
-       if email_to_censor.find(word) >= 0 : 
-          negative_word_count += 1
-          if negative_word_count > 2 : 
-            email_to_censor = email_to_censor.replace(word, "------")
-    return email_to_censor
+            #Censoring the Targeted word
+            word_clean = input_text_words[i]
+            censored_word = ""
+            for x in punctuation : 
+                word_clean = word_clean.strip(x)
+            for x in range(0, len(word_clean)) : 
+                censored_word = censored_word + "-"
+            input_text_words[i] = input_text_words[i].replace(word_clean, censored_word)
 
-def censor_everything(term_list, negative_word_list, text) : 
-    email_to_censor = text.lower()
-    all_terms = term_list + negative_word_list
-    print(all_terms)
-    print(email_to_censor)
-    email_to_censor_lines = email_to_censor.split(".")
-    partially_censored_email = []
-    for line in email_to_censor_lines : 
-        email_to_censor_words = line.split()
-        for word in email_to_censor_words :
-            censored_next_word = False
-            censored_word = False
-            sanatized_word = word.strip(",").strip("(").strip(")")
-            for term in all_terms : 
-                if sanatized_word == term :
-                    censored_word = True
-                    break
-            if censored_word : 
-                partially_censored_email[-1] = "-" * len(partially_censored_email[-1])
-                partially_censored_email.append("-" * len(word))
-                partially_censored_email.append("-" * len(word))
-                censored_next_word = True
-            else :
-                if censored_next_word : 
-                   break
-                partially_censored_email.append(word)
-    return " ".join(partially_censored_email)
+            # Censoring the word before
+            word_before = input_text_words[i-1]
+            for x in punctuation : 
+                word_before = word_before.strip(x)
+            censored_word_before = ""
+            for x in range(0, len(word_before)) : 
+                censored_word_before = censored_word_before + "-"
+            input_text_words[i-1] = input_text_words[i-1].replace(word_before, censored_word_before)
+
+            # Censoring the word After
+            word_before = input_text_words[i+1]
+            for x in punctuation : 
+                word_before = word_before.strip(x)
+            censored_word_before = ""
+            for x in range(0, len(word_before)) : 
+                censored_word_before = censored_word_before + "-"
+            input_text_words[i+1] = input_text_words[i+1].replace(word_before, censored_word_before)
+    return " ".join(input_text_words)
 
 #print("What would you like to remove Mr.Cloudy?")
-#key_words = input()
-#print(censor_word(email_one, key_words))
+##key_words = input()
+#print(censor_word(email_one,"learning algorithm"))
 #print(censor_proprietary_terms(proprietary_terms, email_two))
-#print(censor_negative_word(proprietary_terms, negative_words, email_three))
-print(censor_everything(proprietary_terms, negative_words, email_three))
+#print(censor_negative_word_two(email_three, proprietary_terms, negative_words))
+print(censor_everything(email_four, (negative_words + proprietary_terms)))
